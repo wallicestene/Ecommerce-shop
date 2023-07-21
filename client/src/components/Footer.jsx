de435a6b62d1216af7c0cde87a21af0311f1c311
Your Name <wallicestenewaweru@gmail.com>
2023-07-21 21:37:37 +0300

    footer = complete

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,13 @@
-import { GitHub, Instagram, Twitter } from "@mui/icons-material";
+import { Favorite, GitHub, Instagram, Twitter, YouTube } from "@mui/icons-material";
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const Footer = () => {
   return (
-    <footer className="text-ebony-50  bg-black py-8 px-1 lg:px-10 ">
+    <footer className="text-ebony-50  bg-black py-8 px-1 lg:px-10 font-Montserrat ">
       <div className="  lg:grid lg:grid-cols-4 grid-cols-2 lg:gap-5 gap-8 place-items-center flex flex-row flex-wrap">
         <div>
-          <h1 className=" lg:text-2xl text-lg text-ebony-50 my-1 font-semibold">
+          <h1 className=" lg:text-2xl text-lg my-1 font-semibold text-orange-500">
             E-Shop
           </h1>
           <div className=" lg:text-sm text-xs  text-neutral-500 flex flex-col gap-2 overflow-ellipsis">
@@ -59,24 +60,34 @@ const Footer = () => {
         </div>
       </div>
       <div className="flex items-center justify-center gap-3 my-5 text-neutral-400">
-
-        <a href="https://twitter.com/wallicestene">
-          <div className=" h-8 w-8 grid place-items-center border border-ebony-50 rounded-full">
-            <Twitter fontSize="small"/> 
-          </div>
-        </a>
         <a href="https://github.com/wallicestene">
           <div className=" h-8 w-8 grid place-items-center border border-ebony-50 rounded-full">
-            <GitHub fontSize="small"/> 
+            <GitHub fontSize="small" />
+          </div>
+        </a>
+        <a href="https://twitter.com/wallicestene">
+          <div className=" h-8 w-8 grid place-items-center border rounded-full">
+            <Twitter fontSize="small" />
           </div>
         </a>
         <a href="https://instagram.com/wallicestene">
           <div className=" h-8 w-8 grid place-items-center border border-ebony-50 rounded-full">
-            <Instagram fontSize="small"/> 
+            <Instagram fontSize="small" />
           </div>
         </a>
-      
-      
+        <a href="https://github.com/wallicestene">
+          <div className=" h-8 w-8 grid place-items-center border rounded-full">
+            <YouTube fontSize="small" />
+          </div>
+        </a>
+      </div>
+      <div  className=" lg:text-sm text-xs  text-neutral-500 grid place-items-center">
+        <p>
+        &copy; 2023 <Link to="/" className=" text-orange-500">E-Shop</Link> All rights reserved
+        </p>
+        <p className=" my-1">
+          Made with <span className=" text-red-600"><Favorite fontSize="small"/></span> By Wallicestene Waweru
+        </p>
       </div>
     </footer>
   );
