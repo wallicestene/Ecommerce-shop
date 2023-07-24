6078e8f8203d46d0a624f20808d7b4db8dfe8350
Your Name <wallicestenewaweru@gmail.com>
2023-07-24 13:54:33 +0300

    first commit

--- client/src/components/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import AllProducts from "./AllProducts";
+import { Skeleton } from "@mui/material";
 
 const FeaturedProducts = () => {
   const [categories, setCategories] = useState([]);
@@ -8,13 +9,13 @@ const FeaturedProducts = () => {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
   const [selectedCategory, setSelectedCategory] = useState(null);
-
+  const skeletonArr = [1, 2, 3, 4]
   useEffect(() => {
     // Fetch categories
     fetch("https://e-shop-xlam.onrender.com/product/categories")
       .then((res) => {
         if (!res.ok) {
-          throw new Error("Error when fetching categories");
+          throw new Error("Error when fetching categories!");
         } else {
           return res.json();
         }
@@ -76,9 +77,13 @@ const FeaturedProducts = () => {
         Discover our handpicked collection of trendy and stylish clothing, showcasing the latest fashion must-haves for men, women, and kids on our featured products page.
         </p>
       </div>
-      <div className="categories flex flex-row lg:gap-10 items-center justify-center flex-wrap w-full text-zinc-500">
-        {loading && <p>Loading...</p>}
-        {error && <p>error</p>}
+      <div className="categories flex flex-row lg:gap-10 gap-2 items-center justify-center flex-wrap w-full text-zinc-500">
+        {loading && 
+        skeletonArr.map((index) => (
+          <Skeleton key={index} variant="rounded" width={90} height={30} />
+        ))
+        }
+        {error && <p>{error}</p>}
         <div
           onClick={() => setFilteredProducts(products)}
           className="h-8  w-fit px-5 py-1 hover:rounded-full hover:bg-orange-500 hover:text-white cursor-pointer focus:bg-orange-500 delay-150 duration-300 "
@@ -101,7 +106,9 @@ const FeaturedProducts = () => {
           ))}
       </div>
       <div className="grid lg:grid-cols-4 grid-cols-2 md:grid-cols-3 lg:gap-5 gap-5 my-5 ">
-        {loading && <p>Loading...</p>}
+        {loading && skeletonArr.map((index) => (
+          <Skeleton key={index} variant="rectangle" width={150} height={240} />
+        ))}
         {error && <p>error</p>}
         {!loading &&
           filteredProducts.map((product, index) => (
